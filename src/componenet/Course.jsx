7ead9723a300547bc2e561be2e4a66b15a86990e
Bikash Kumar <bkbikas0@gmail.com>
2024-11-21 02:12:20 +0530

    this is my web front and backend

--- src/componenet/Course.jsx
+++ src/componenet/Course.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import list from "../../public/list.json";
+import React, { useEffect ,useState } from "react";
+// import list from "../../public/list.json";
 import Cards from "../componenet/Cards";
 import { Link } from "react-router-dom";
-
+import axios from "axios"
 function Course() {
+ const  [book, setBook] = useState([])
+ useEffect(()=>{
+  const getBook=async()=>{
+    try {
+      const res=await axios.get("http://localhost:4001/book");
+      console.log(res.data);
+      setBook(res.data)
 
+
+    } catch (error) {
+      console.log("error",error);
+
+
+    }
+  }
+  getBook()
+ },[])
 
   return (
     <>
@@ -30,7 +46,7 @@ function Course() {
           </Link>
         </div>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-3">
-          {list.map((item) => (
+          {book.map((item) => (
             <Cards key={item.id} item={item} />
           ))}
         </div>
--- src/componenet/FreeBook.jsx
+++ src/componenet/FreeBook.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React  from 'react'
+import { useState,useEffect } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from '../../public/list.json'
+// import list from '../../public/list.json'
 import Cards from './Cards';
+import axios from 'axios';
 
 function FreeBook() {
-	const filterData=list.filter((data)=>data.category==="Free")
+	const  [book, setBook] = useState([])
+	useEffect(()=>{
+	 const getBook=async()=>{
+	   try {
+		 const res=await axios.get("http://localhost:4001/book");
+
+		const data= res.data.filter((data)=>data.category==="Free")
+		// console.log(data);
+		setBook(data);
+
+
+
+	   } catch (error) {
+		 console.log("error",error);
+
+
+	   }
+	 }
+	 getBook()
+	},[])
+	// const filterData=list.filter((data)=>data.category==="Free")
 	// console.log(filterData);
 	var settings = {
 		dots: true,
@@ -54,7 +76,7 @@ function FreeBook() {
 
 <div>
 <Slider {...settings}>
-       {filterData.map((item)=>(<Cards item={item} key={item.id}/>))}
+       {book.map((item)=>(<Cards item={item} key={item.id}/>))}
       </Slider>
 </div>
 </div>
